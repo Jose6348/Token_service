c9351235c2e7c97a44aa12ff1bcdc86911230f94
Jose6348 <jfalasco88@gmail.com>
2025-02-24 23:37:39 -0300

    refact Go

--- main.go
+++ main.go
@@ -1,41 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	csrfToken := csrf.Token(r)
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<html><body><h1>Servidor Protegido CSRF</h1><form action='/submit' method='POST'><input type='hidden' name='csrf_token' value='%s'><input type='text' name='data'><input type='submit' value='Enviar'></form></body></html>", csrfToken)
-}
-
-func submitHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
-		return
-	}
-	data := r.FormValue("data")
-	fmt.Fprintf(w, "Dados recebidos com sucesso: %s", data)
-}
-
 func main() {
-	// Criando um middleware CSRF
-	csrfMiddleware := csrf.Protect([]byte("32-byte-secret-key"), csrf.Secure(false))
+	db := SetupDatabase()
+	defer db.Close()
 
-	// Criando roteador
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler).Methods("GET")
-	r.HandleFunc("/submit", submitHandler).Methods("POST")
+	csrfMiddleware := csrf.Protect([]byte("32-byte-long-auth-key"))
 
-	// Aplicando middleware CSRF
-	http.Handle("/", csrfMiddleware(r))
+	r.HandleFunc("/conta", CriarConta).Methods("POST")
+	r.HandleFunc("/conta/{id}/saldo", ConsultarSaldo).Methods("GET")
+	r.HandleFunc("/conta/{id}/deposito", DepositarDinheiro).Methods("POST")
+	r.HandleFunc("/conta/{id}/saque", SacarDinheiro).Methods("POST")
+	r.HandleFunc("/conta/transferencia", TransferirDinheiro).Methods("POST")
+	r.HandleFunc("/conta/{id}", FecharConta).Methods("DELETE")
 
-	log.Println("Servidor rodando em http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Servidor iniciado na porta 8080")
+	http.ListenAndServe(":8080", csrfMiddleware(r))
 }
